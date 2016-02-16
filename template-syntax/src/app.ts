6e4252dd79011e2a316b6a27f6836e2e5b3fad2f
lewis617 <897473127@qq.com>
2016-02-16 13:40:24 +0800

    template syntax

---
+++ template-syntax/src/app.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+    selector: 'app',
+    template: require('./app.html')
+})
+export class App {
+    firstName: string = 'lewis';
+    doSomething($event){
+        console.log('点击了这个按钮：',$event.target);
+    }
+    callPhone(val){
+        console.log('局部变量phone的值：',val);
+    }
+    callFax(val){
+        console.log('局部变量fax的值：',val);
+    }
+    isActive:boolean=true;
+}
+
+bootstrap(App);
